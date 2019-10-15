7cb1cbcd9f4f6ccdf2740a0090da4959f51460b2
Killian Kemps <developer@killiankemps.fr>
2019-10-15 11:24:12 +0200

    Factorize and display pretty links

--- main.rb
+++ main.rb
@@ -2,48 +2,72 @@
 
 require 'socket'
 require 'timeout'
+require 'colorize'
 
 host = 'gopherpedia.com'
+host = 'khzae.net'
 port = 70
 path = '/'
 request = "#{path}\r\n"
 
-begin
-  Timeout.timeout(3) do
-    socket = TCPSocket.open(host, port)
-    socket.print(request)
-    response = socket.read
-    socket.close
-    lines = response.split("\n")
-
-    # Handle the final dot if it is there
-    if lines[-1].strip == "." then
-      lines = lines[0..-2]
+def request_maker(host, port, request)
+  begin
+    Timeout.timeout(3) do
+      socket = TCPSocket.open(host, port)
+      socket.print(request)
+      response = socket.read
+      socket.close
+      return response
     end
+  rescue Timeout
+    puts 'timed out!'
+  end
+end
 
-    parsed_lines = []
+def response_parser(response)
+  lines = response.split("\n")
 
-    lines.each do |line|
-      type = line.slice!(0)
+  # Handle the final dot if it is there
+  if lines[-1].strip == "." then
+    lines = lines[0..-2]
+  end
 
-      # Skip lines with \r
-      next if type == "\r"
+  parsed_lines = []
 
-      splitted_line = line.split("\t")
+  lines.each do |line|
+    type = line.slice!(0)
 
-      parsed_line = {
-        type: type,
-        description: splitted_line[0],
-        path: splitted_line[1],
-        host: splitted_line[2],
-        port: splitted_line[3].to_i
-      }
+    # Skip lines with \r
+    next if type == "\r"
 
-      parsed_lines << parsed_line
+    splitted_line = line.split("\t")
 
-      puts(parsed_line[:description])
+    parsed_line = {
+      type: type,
+      description: splitted_line[0],
+      path: splitted_line[1],
+      host: splitted_line[2],
+      port: splitted_line[3].to_i
+    }
+
+    parsed_lines << parsed_line
+  end
+
+  parsed_lines
+end
+
+def display(parsed_response)
+  parsed_response.each do |line|
+    if line[:path] != '' then
+      puts(line[:description].blue)
+    else
+      puts(line[:description])
     end
   end
-rescue Timeout
-  puts 'timed out!'
 end
+
+response = request_maker(host, port, request)
+
+parsed_response = response_parser(response)
+
+display(parsed_response)
